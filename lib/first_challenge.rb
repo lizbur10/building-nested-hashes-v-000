8fba55b26b6429929806608ff2beee02d069884e
Liz Burton <liz.burton147@gmail.com>
2017-11-25 16:00:25 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,18 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy =
+   { "montagues" => {}, "capulets" => {} }
+
+
+
+
+
+
+   }
 end
+
+
+# { "montagues" => "Patriarch" => { name: "Lord Montague", age: 53 },
+#               => "Matriarch" => { name: "Lady Montague", age: 54 },
+#               => "Hero"      => { name: "Romeo", age: 15, status: "alive"},
+#                              => "Hero's Friends" => { { "Friend 1" => { name: "Benvolio", age: 17, attitude: "worried" } },
+#                              => { "Friend 2" => { name: "Mercutio", age: 18, attitude: "hot-headed"} }
